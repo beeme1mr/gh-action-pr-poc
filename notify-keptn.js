bcef0e8901996937dc3e858cd303446d6ce652f4
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-08-29 13:33:43 -0400

    test

--- notify-keptn.js
+++ notify-keptn.js
@@ -1,4 +1,24 @@
-module.exports = ({ github, context }) => {
+module.exports = ({ context }) => {
   const labels = context.payload.pull_request.labels;
-  console.log(labels);
+  const parsedLabels = {};
+  labels.forEach((label) => {
+    const [key, value] = label.name.split(":");
+    parsedLabels[key] = value;
+  });
+
+  return {
+    data: {
+      project: parsedLabels["project"],
+      stage: parsedLabels["stage"],
+      service: parsedLabels["service"],
+      status: "succeeded",
+      result: "pass",
+    },
+    source: "github",
+    specversion: "1.0",
+    // TODO see what the event name is
+    type: "sh.keptn.event.mytask-active.finished",
+    shkeptncontext: parsedLabels["shkeptncontext"],
+    triggeredid: parsedLabels["triggeredid"],
+  };
 };
